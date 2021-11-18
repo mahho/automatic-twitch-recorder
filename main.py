0340d69c41a646fe846f141dab7eab80c4796df0
Darek M <mahho@mahho.net>
2021-11-18 11:49:18 +0100

    autostart recorder when usernames are added via argv parameter

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import threading
 
 import ATRHandler
 import utils
+import sys
 from atr_cmd import AtrCmd
 from daemon import Daemon
 
@@ -9,4 +10,14 @@ if __name__ == '__main__':
     utils.get_client_id()  # creates necessary config before launch
     server = Daemon(('127.0.0.1', 1234), ATRHandler.ATRHandler)
     threading.Thread(target=server.serve_forever).start()
+
+    if len(sys.argv) > 1:
+        for argnum in range(1, len(sys.argv)):
+            username = sys.argv[argnum]
+            print("> Adding", username)
+            server.add_streamer(username)
+        print("> Autostarting...")
+        server.start()
+
+
     AtrCmd().cmdloop_with_keyboard_interrupt()
